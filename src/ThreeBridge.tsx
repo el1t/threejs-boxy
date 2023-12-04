6e2e14c9e10d9efb5abe3d1eb5a63daf580ec495
Ellis Tsung <git@tsung.co>
2023-12-03 20:27:20 -0800

    Add threejs sample

--- src/ThreeBridge.tsx
+++ src/ThreeBridge.tsx
@@ -26,6 +26,30 @@ const ThreeBridge: React.FC = () => {
 		}
 	}, [renderer])
 
+	// Threejs sample cube
+	useEffect(() => {
+		const scene = new THREE.Scene()
+		const camera = new THREE.PerspectiveCamera(
+			75,
+			window.innerWidth / window.innerHeight,
+			0.1,
+			1000,
+		)
+
+		const geometry = new THREE.BoxGeometry(1, 1, 1)
+		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+		const cube = new THREE.Mesh(geometry, material)
+		scene.add(cube)
+
+		camera.position.z = 5
+
+		const render = () => {
+			requestAnimationFrame(render)
+			renderer.render(scene, camera)
+		}
+		render()
+	}, [renderer])
+
 	return <div ref={useMergeRefs(ref, callbackRef)} />
 }
 
